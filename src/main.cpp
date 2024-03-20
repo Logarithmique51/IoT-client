4aab2a3534c7570d380644d1ad58775ed2fedefb
Logarithmique <suivengbrandon@gmail.com>
2024-03-20 02:33:08 +0100

    AUTO DISCOVER

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,72 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
-// Update these with values suitable for your network and MQTT broker
 const char* ssid = "Freebox44 2.4GHZ";
 const char* password = "noel2022";
-const char* mqtt_server = "192.168.1.83";
-const int mqtt_port = 1883; // Default MQTT port
-const char* mqtt_topic = "your_topic";
+const char* mqtt_server = "192.168.1.29";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "limitlesslogic/ping";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-//   WiFi.begin("Freebox44 2.4GHZ", "noel2022");
 
 void setup_wifi() {
-  delay(10);
-  // Connect to Wi-Fi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.println("ATTEMPT CONNECT");
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
     if (client.connect("ESP8266Client")) {
+      client.subscribe("maison/lampe/1234");
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message reçu sur le topic: ");
+  Serial.println(topic);
+  Serial.print("Payload: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void pingBroker(){
+  
+  char output[128];
+  JsonDocument descriptor;
+  descriptor["mac"] = WiFi.macAddress();
+  descriptor["ip"] = WiFi.localIP();
+  descriptor["strenght"] = WiFi.RSSI();
+  descriptor["type"] = "SHUTTER"; 
+  descriptor["id"] = system_get_chip_id();
+  serializeJson(descriptor, output);
+
+  client.publish(mqtt_topic,output);
+
 }
 
 void setup() {
   Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
@@ -60,9 +74,9 @@ void loop() {
     reconnect();
   }
   client.loop();
-  // Publish a message every 5 seconds
-  client.publish(mqtt_topic, "Hello from ESP8266");
+  pingBroker();
   delay(5000);
+ 
 }
 
 // #include <ESP8266WiFi.h>
